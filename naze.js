73bf13528c831cf37a35e02afcee846ddd702b30
ikhsan77 <66218401+ikhsan77@users.noreply.github.com>
2022-10-07 12:56:53 +0700

    Update naze.js

--- naze.js
+++ naze.js
@@ -1466,7 +1466,7 @@ ${vote[m.chat][2].map((v, i) => `├ ${i + 1}. @${v.split`@`[0]}`).join('\n')}
                 m.reply(mess.wait)
                 
 		let anu = await fetchJson(api('lolhuman', '/instagram', { url: isUrl(text)[0] }, 'apikey'))
-	    	for (let media of anu.result[0) shann.sendFileUrl(m.chat, media, `Download Url Instagram From ${isUrl(text)[0]}`, m)
+	    	for (let media of anu.result[0]) shann.sendFileUrl(m.chat, media, `Download Url Instagram From ${isUrl(text)[0]}`, m)
             }
             break
 
